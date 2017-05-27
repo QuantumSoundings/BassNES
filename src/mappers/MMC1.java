795ab07858e737c090e2fef8fc629a9441be15f6
QuantumSoundings <jshowe95@gmail.com>
2017-05-27 10:54:07 -0500

    Fixed MMC1 for sorom, surom, and sxrom.

--- src/mappers/MMC1.java
+++ src/mappers/MMC1.java
@@ -10,17 +10,23 @@ public class MMC1 extends Mapper {
 	int Mirror_mode;
 	byte[][] PRGbanks;
 	byte[][] CHRbanks;
+	int banknumber;
+	int lastbank;
+	boolean bigrom;
+	boolean smallchrrom;
+	int lastwrite=0;
+	boolean prgramdisable;
 	public MMC1(){
 		super();
 		System.out.println("Mapper 1 (SNROM) Fully Supported!"); 
 		PRG_RAM = new byte[0x2000];
 	}
-	int lastwrite=0;
+	
 	@Override
 	public void cartridgeWrite(int index, byte b){
-		if(index>=0x6000&&index<0x8000)
+		if(index>=0x6000&&index<0x8000&&!prgramdisable)
 			PRG_RAM[index%0x2000]=b; 
-		if(lastwrite == nes.cpuclock-1)
+		if(lastwrite == cpuclock-1)
 			return;
 		if(index>=0x8000&&index<=0xffff){
 			if(b<0){
@@ -28,7 +34,7 @@ public class MMC1 extends Mapper {
 				shiftregister= ((Mirror_mode)|(PRG_ROM_mode<<2)|(CHR_ROM_mode<<4))|0xc;
 				writeRegister(0x8000);
 				shiftregister= 0b10000;
-				PRG_ROM[1] = PRGbanks[PRGbanks.length-1];
+				PRG_ROM[1] = PRGbanks[lastbank];
 			}
 			else if((shiftregister&1)==0){
 				shiftregister>>=1;
@@ -42,10 +48,11 @@ public class MMC1 extends Mapper {
 				writeRegister(index);
 				shiftregister = 0b10000;
 			}
-			lastwrite = nes.cpuclock;
+			lastwrite = cpuclock;
 		}	
 		
 	}
+	
 	@Override
 	public void setPRG(byte[] prg){
 		PRGbanks = new byte[prg.length/0x4000][0x4000];
@@ -54,6 +61,13 @@ public class MMC1 extends Mapper {
 		}
 		PRG_ROM[0]=PRGbanks[0];
 		PRG_ROM[1]=PRGbanks[PRGbanks.length-1];
+		if((prg.length/0x400)==512){
+			bigrom = true;
+			lastbank = 0xf;
+		}
+		else{
+			lastbank = PRGbanks.length-1;
+		}
 	}
 	@Override
 	public void setCHR(byte[] chr){
@@ -69,6 +83,8 @@ public class MMC1 extends Mapper {
 			CHR_ROM[1]= new byte[0x1000];
 			CHR_ram = true;
 		}
+		smallchrrom = CHR_ram || CHRbanks.length==2;
+		
 	}
 	@Override
 	byte cartridgeRead(int index){
@@ -82,6 +98,7 @@ public class MMC1 extends Mapper {
 	}
 	public void writeRegister(int index){
 		if(index>=0x8000&&index<=0x9fff){// Control register
+			//System.out.println("Control reg write "+Integer.toBinaryString(shiftregister));
 			Mirror_mode = shiftregister&0b11;
 			switch(Mirror_mode){
 			case 0:
@@ -95,7 +112,7 @@ public class MMC1 extends Mapper {
 			}
 			PRG_ROM_mode = (shiftregister&0b1100)>>2;
 			CHR_ROM_mode = (shiftregister&0b10000)>>4;
-			//System.out.println("Writing control reg with "+Integer.toBinaryString(shiftregister));
+			updatePRGbanks();
 		}
 		else if(index>=0xa000&&index<=0xbfff){// CHR bank 0 select
 			if(!CHR_ram)
@@ -105,33 +122,63 @@ public class MMC1 extends Mapper {
 				}
 				else
 					CHR_ROM[0]=CHRbanks[shiftregister&(CHRbanks.length-1)];
+			if(bigrom){
+				prgswitch = ((shiftregister&0b10000)>0?0b10000:0);
+				banknumber&=0xf;banknumber|=prgswitch;
+				lastbank&=0xf;lastbank|=prgswitch;
+				updatePRGbanks();
+			}
 		}
 		else if(index>=0xc000&&index<=0xdfff){// CHR bank 1 select
 			if(!CHR_ram)
 				if(CHR_ROM_mode==1)
 					CHR_ROM[1]=CHRbanks[shiftregister&(CHRbanks.length-1)];
-		}
-		else if(index>=0xe000&&index<=0xffff){
-			switch(PRG_ROM_mode){
-			case 0: case 1:
-				//System.out.println("Switching prgrom case 0/1");
-				PRG_ROM[0] = PRGbanks[(shiftregister&0b1110)&(PRGbanks.length-1)];
-				PRG_ROM[1] = PRGbanks[(((shiftregister&0b1110))&(PRGbanks.length-1))+1];
-				break;
-			case 2:
-				//System.out.println("Switching prgrom case 2");
-
-				PRG_ROM[0]= PRGbanks[0];
-				PRG_ROM[1] = PRGbanks[(shiftregister&0b1111)&(PRGbanks.length-1)];
-				break;
-			case 3:
-				//System.out.println("Switching prgrom case 3");
-
-				PRG_ROM[0] = PRGbanks[(shiftregister&0b1111)&(PRGbanks.length-1)];
-				PRG_ROM[1] = PRGbanks[PRGbanks.length-1];
-				break;
+			if(bigrom&&CHR_ROM_mode==1){
+				prgswitch = ((shiftregister&0b10000)>0?0b10000:0);
+				banknumber&=0xf;banknumber|=prgswitch;
+				lastbank&=0xf;lastbank|=prgswitch;
+				updatePRGbanks();
 			}
 		}
+		else if(index>=0xe000&&index<=0xffff){
+			banknumber = shiftregister&0xf;
+			updatePRGbanks();
+			
+		}
 		
+	}
+	private void updatePRGbanks(){
+		switch(PRG_ROM_mode){
+		case 0: case 1:
+			//System.out.println("Switching prgrom case 0/1");
+			PRG_ROM[0] = PRGbanks[(banknumber)&(lastbank)];
+			PRG_ROM[1] = PRGbanks[((banknumber)&(lastbank))+1];
+			break;
+		case 2:
+			//System.out.println("Switching prgrom case 2");
+			PRG_ROM[0]= PRGbanks[0];
+			PRG_ROM[1] = PRGbanks[(banknumber)&(lastbank)];
+			break;
+		case 3:
+			//System.out.println("Switching prgrom case 3 "+prgswitch+" "+ ((shiftregister&0b1111)|prgswitch));
+			PRG_ROM[0] = PRGbanks[(banknumber)&(lastbank)];
+			PRG_ROM[1] = PRGbanks[lastbank];
+			break;
+			
+		}
+	}
+	int prgswitch;
+	int cpuclock;
+	@Override
+	public void runFrame() {
+		while(!ppu.doneFrame){
+			ppu.doCycle();
+			ppu.doCycle();
+			ppu.doCycle();
+			cpu.run_cycle();
+			cpuclock++;
+			apu.doCycle();	
+		}
+		ppu.doneFrame=false;
 	}
 }
