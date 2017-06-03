4a68fe8f755981bddb570a3e295c458e926c8997
QuantumSoundings <jshowe95@gmail.com>
2017-06-03 13:29:29 -0500

    Tests

--- src/ui/ControlUI.java
+++ src/ui/ControlUI.java
@@ -32,7 +32,7 @@ public class ControlUI extends JFrame {
         //noinspection MagicConstant
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 313);
-		contentPane = new JPanel();
+		contentPane = new JPanel();  
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
