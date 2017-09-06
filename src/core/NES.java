3f8fc22be997c6b68d1de5b330621170dd81c565
QuantumSoundings <jshowe95@gmail.com>
2017-09-06 17:33:29 -0500

    Flushed out the frame timing idea a bit more.
    Fixes initial wrong behavior regarding unlimited frame rate mode and
    polite frame timing.

--- src/core/NES.java
+++ src/core/NES.java
@@ -271,24 +271,27 @@ public class NES implements Runnable,NESAccess {
 		frameStartTime = System.nanoTime();
 		map.runFrame();
 		frameStopTime = System.nanoTime() - frameStartTime;
-		if(frameStopTime<flextimer&&NesSettings.frameLimit)
+		if(NesSettings.politeFrameTiming?frameStopTime<flextimer:frameStopTime<16500000&&NesSettings.frameLimit)
 			try {
 				int waittime = (int) (flextimer-frameStopTime);
-				Thread.sleep(waittime/1000000,waittime%1000000 );
-				//while(System.nanoTime()-frameStartTime<16500000){
-				//	if(NesSettings.politeFrameTiming)
-				//		Thread.sleep(0,10000);
-				//}
+				if(NesSettings.politeFrameTiming)
+					Thread.sleep(waittime/1000000,waittime%1000000 );
+				else
+					while(System.nanoTime()-frameStartTime<16500000){
+					//if(NesSettings.politeFrameTiming)
+						//Thread.sleep(0,10000);
+					}
 			} catch ( InterruptedException e){
 				e.printStackTrace();
 			}
 		if(framecount%60==0){
 			double x = 1000.0/(System.currentTimeMillis()-fpsStartTime);
 			currentFPS = x*60;
-			if(currentFPS>60.1)
-				flextimer+=50000;
-			else
-				flextimer-=50000;
+			if(NesSettings.frameLimit&&NesSettings.politeFrameTiming)
+				if(currentFPS>60.1)
+					flextimer+=50000;
+				else
+					flextimer-=50000;
 			fpsStartTime=System.currentTimeMillis();
 		}
 		framecount++;
